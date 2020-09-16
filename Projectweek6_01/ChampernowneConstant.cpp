2c2870cf61132214a2237325a036a1e42f14b742
Aon Jantaraweeragul <63010487@kmitl.ac.th>
2020-09-16 17:04:13 +0700

    Add project files.

---
+++ Projectweek6_01/ChampernowneConstant.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+	std::string d;
+	int i = 0;
+	int pro = 1;
+	do
+	{
+		d += std::to_string(i);
+		i++;
+	} while (d.size() <= 1000000);
+	{
+		for (i = 1; i <= 1000000; i *= 10)
+		{
+			pro *= d[i] - '0';
+		}
+	}
+	std::cout << pro << std::endl;
+}
