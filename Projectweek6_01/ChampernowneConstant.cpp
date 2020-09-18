547a30d1ab74929951bd3ceed85c5dc949aa48f3
Aon Jantaraweeragul <63010487@kmitl.ac.th>
2020-09-18 21:21:35 +0700

    edit

--- Projectweek6_01/ChampernowneConstant.cpp
+++ Projectweek6_01/ChampernowneConstant.cpp
@@ -6,13 +6,16 @@ int main()
 	std::string d;
 	int i = 0;
 	int pro = 1;
+	int range;
+	cout << "Enter range : ";
+	cin >> range; //range is 100,000 from question
 	do
 	{
 		d += std::to_string(i);
 		i++;
-	} while (d.size() <= 1000000);
+	} while (d.size() <= range);
 	{
-		for (i = 1; i <= 1000000; i *= 10)
+		for (i = 1; i <= range; i *= 10)
 		{
 			pro *= d[i] - '0';
 		}
